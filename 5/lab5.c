4ff7f726a67aa0fbca39882014c7daed0896105d
grejioh <grejioh@outlook.com>
2021-05-13 23:25:02 +0800

    add 5

---
+++ 5/lab5.c
@@ -0,0 +1,13 @@
+// 参考https://www.cnblogs.com/sparkdev/p/11008978.html
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define FIFO_NAME "/tmp/testp"
+#define BUFFER_SIZE 10000
+#define
+int main() { return 0; }
